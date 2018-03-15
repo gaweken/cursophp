3e21020fa42afb87347b242a0388c0c45e694fb6
Gaweken <felipeanchieta@live.com>
2018-03-15 19:19:42 -0300

    Incluida a busca pelo login e retornando um json

--- dao/exemplo-01.php
+++ dao/exemplo-01.php
@@ -4,13 +4,24 @@ require_once("Config.php");
 /*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios);*/
+echo json_encode($usuarios);
 
+//Trazendo apenas um usuario pelo ID
 $usuario = new Usuario();
 
 $usuario->loadById(2);
 
-echo $usuario;
+echo $usuario;*/
 
+/*Trazendo uma lista de usuarios com um metodo estatico
+$lista = Usuario::getList();
 
+echo json_encode($lista);
+*/
+
+//Carrega uma lista de usuarios buscando pelo login
+
+$search = Usuario::search('u');
+
+echo json_encode($search);
 ?>
