2e6a2f1abe4caf7db27c49330bd776324168b481
Gaweken <felipeanchieta@live.com>
2018-03-15 19:17:33 -0300

    Adicionados os metodos getList() e searcg()

--- dao/class/Usuario.php
+++ dao/class/Usuario.php
@@ -28,6 +28,16 @@ class usuario {
 		}
 	}
 
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(":SEARCH"=>"%".$login."%"));
+	}
+
 	public function __toString(){
 		return json_encode(array(
 			"idusuario"=>$this->idusuario,
